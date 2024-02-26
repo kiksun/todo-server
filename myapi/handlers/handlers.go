d8d05787557449df570a91b9b66dab7437c39392
kiksun <kimera4173@gmail.com>
2024-02-26 22:35:27 +0900

    add gollira

--- myapi/handlers/handlers.go
+++ myapi/handlers/handlers.go
@@ -1,21 +1,60 @@
 package handlers
 
 import (
+	"fmt"
 	"io"
-	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
+// /hello のハンドラ
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		log.Println("Get")
-	}
-
-	if req.Method == http.MethodPost {
-		log.Println("Post")
-	}
-	if req.Method == http.MethodPut {
-		log.Println("Put")
-	}
 	io.WriteString(w, "Hello, world!\n")
+}
+
+// /article のハンドラ
+func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Posting Article...\n")
+}
+
+// /article/list のハンドラ
+func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
+	queryMap := req.URL.Query()
+	var page int
+	
+	if p, ok := queryMap["page"]; ok && len(p) > 0 {
+		var err error
+		page, err = strconv.Atoi(p[0])
+		if err != nil {
+			http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+			return
+		}
+	} else {
+		page = 1
+	}
+	resString := fmt.Sprintf("Article List (page %d)\n", page)
+	io.WriteString(w, resString)
+}
+
+// GET /article/{id} のハンドラ
+func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
+	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
+	if err != nil {
+		http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+		return
+	}
+	resString := fmt.Sprintf("Article No.%d\n", articleID)
+	io.WriteString(w, resString)
+}
+
+// /article/nice のハンドラ
+func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Posting Nice...\n")
+}
+
+// /comment のハンドラ
+func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Posting Comment...\n")
 }
--- myapi/main.go
+++ myapi/main.go
@@ -1,16 +1,45 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/kiksun/todo-server/handlers"
 )
 
+type Todo struct {
+	CommentID int
+	ArticleID int
+	content   string
+}
+
 func main() {
+	todo1 := Todo{
+		CommentID: 1,
+		ArticleID: 2,
+		content:   "aaaaaa",
+	}
+	todoList := []Todo{todo1, todo1}
 
-	http.HandleFunc("/", handlers.HelloHandler)
+	jsonData, err := json.Marshal(todoList)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("%s\n", jsonData)
 
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article", handlers.PostArticleHandler)
+
+	r.HandleFunc("/article/list", handlers.ArticleListHandler)
+	r.HandleFunc("/article/{id:[0-9]+}",
+		handlers.ArticleDetailHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/nice", handlers.PostNiceHandler)
+	r.HandleFunc("/comment", handlers.PostCommentHandler)
 	log.Println("server start at port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
